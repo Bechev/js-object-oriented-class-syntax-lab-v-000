3916296864ee011e2648cb23590dc087024bdffe
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-18 15:35:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+class BoardMember{
+  constructor(name, homeState, training){
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+  }
+
+  veto{
+    return `No, I must disagree` 
+  }
+  approve{
+    return `You can do that!`
+  }
+
+  doCharity{
+      return `I like to help people.`
+  }
+}
+
+
